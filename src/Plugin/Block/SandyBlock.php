0fc7cce58730d73fa0a25fc0bdf07219d1c26246
RealLukeMartin <lukemartinwebdev@gmail.com>
2015-06-28 03:18:28 -0500

    build sandy simple block

--- src/Plugin/Block/SandyBlock.php
+++ src/Plugin/Block/SandyBlock.php
@@ -1,18 +1,34 @@
 <?php
-namespace Drupal\sandy\Plugin\Block;
-
-use Drupal\Core\Block\BlockBase;
-
 /**
- * Provides a 'Sandy' block.
+ * @file
+ * Contains \Drupal\sandy\Plugin\Block\SandyBlock.
+ */
+ 
+namespace Drupal\block_example\Plugin\Block;
+ 
+use Drupal\block\BlockBase;
+use Drupal\Component\Annotation\Plugin;
+use Drupal\Core\Annotation\Translation;
+ 
+/**
+ * Provides a 'Sandy: empty block' block.
  *
  * @Block(
- *   id = "sandy_block",
- *   admin_label = @Translation("Sandy Block"),
+ *   id = "sandy_simple_block",
+ *   subject = @Translation("Sandy box block"),
+ *   admin_label = @Translation("Sandy box block")
  * )
  */
-class SandyBlock extends BlockBase {
-
-    // Override BlockPluginInterface methods here.
-
+class BlockSandySimpleBlock extends BlockBase {
+   
+  /**
+   * Implements \Drupal\block\BlockBase::blockBuild().
+   */
+  public function build() {
+    return array(
+      '#markup' => t('Simple Sandy block data'),
+    );
+    return \Drupal::formBuilder()->getForm('Drupal\search\Form\SearchBlockForm');
+  }
+ 
 }
