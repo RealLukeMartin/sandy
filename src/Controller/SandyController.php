e4e2542af247bd169cf986b944f3064d98143482
RealLukeMartin <lukemartinwebdev@gmail.com>
2015-06-28 00:36:36 -0500

    redo the way a link is created

---
+++ src/Controller/SandyController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\sandy\Controller\SandyController.
+ */
+
+namespace Drupal\sandy\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class SandyController extends ControllerBase {
+  public function content() {
+    return array(
+        '#type' => 'markup',
+        '#markup' => t('Sandy Box!'),
+    );
+  }
+}
